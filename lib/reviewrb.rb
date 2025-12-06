80cbfe9e83b537f77e2cc6afd4c9c1fcda5f0188
VictorHSCosta <victorhenriquecosta23@gmail.com>
2025-12-05 22:37:13 -0300

    first commit

---
+++ lib/reviewrb.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "reviewrb/version"
+
+module Reviewrb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/reviewrb_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Reviewrb do
+  it "has a version number" do
+    expect(Reviewrb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
